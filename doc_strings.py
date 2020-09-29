41b101986c221fe882d9c2d78f7d3598e24b06bc
edevars <e.devars.dev@gmail.com>
2020-09-28 22:03:54 -0500

    :sparkles: Doc strings

---
+++ doc_strings.py
@@ -0,0 +1,11 @@
+def add(number_1, number_2):
+    """Add to numbers
+        number_1 is an int
+        number_2 is an int
+        return the add of two numbers
+    """
+
+    return number_1 + number_2
+
+
+help(add)
